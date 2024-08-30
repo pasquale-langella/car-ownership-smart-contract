39303867af6f8ed30fb66703b114a8a5b7c11b52
Pasquale Langella <paslangella@gmail.com>
2024-08-30 15:58:23 +0200

    add fixture to improve test execution time

--- test/CarOwnership.js
+++ test/CarOwnership.js
@@ -1,17 +1,35 @@
 const { expect } = require("chai");
 const { boolean } = require("hardhat/internal/core/params/argumentTypes");
+const {
+    loadFixture,
+  } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe("Car Ownership contract", function () {
-  it("Checks owner assignment in the contract", async function () {
-    const [owner] = await ethers.getSigners();
-    const carOwnershipContract = await ethers.deployContract("CarOwnership");
 
-    expect(await carOwnershipContract.getOwner()).equal(owner);
+  async function deployCarOwnershipFixture() {
+      // Get the Signers here.
+      const [owner, buyer] = await ethers.getSigners();
+  
+      // To deploy our contract, we just have to call ethers.deployContract and await
+      // its waitForDeployment() method, which happens once its transaction has been
+      // mined.
+      const carOwnershipContract = await ethers.deployContract("CarOwnership");
+  
+      await carOwnershipContract.waitForDeployment();
+  
+      // Fixtures can return anything you consider useful for your tests
+      return { carOwnershipContract, owner, buyer};
+    }
+
+  it("Checks owner assignment in the contract", async function () {
+
+    const { carOwnershipContract, owner } = await loadFixture(deployCarOwnershipFixture);
+
+    expect(await carOwnershipContract.getOwner()).equal.to(owner);
   });
 
   it("Checks car build", async function () {
-    const [owner] = await ethers.getSigners();
-    const carOwnershipContract = await ethers.deployContract("CarOwnership");
+    const { carOwnershipContract} = await loadFixture(deployCarOwnershipFixture);
 
     await expect(carOwnershipContract.buildCar("EW722YG"))
     .to.emit(carOwnershipContract, "CarBuilt")
@@ -19,16 +37,14 @@ describe("Car Ownership contract", function () {
   });
 
   it("Checks owner owns car EW722YG after build", async function () {
-    const [owner] = await ethers.getSigners();
-    const carOwnershipContract = await ethers.deployContract("CarOwnership");
+    const { carOwnershipContract, owner } = await loadFixture(deployCarOwnershipFixture);
     
     await carOwnershipContract.buildCar("EW722YG")
     expect(await carOwnershipContract.getCarOwner("EW722YG")).equal(owner)
   });
 
   it("Checks car transfer", async function () {
-    const [owner, buyer] = await ethers.getSigners();
-    const carOwnershipContract = await ethers.deployContract("CarOwnership");
+    const { carOwnershipContract, owner, buyer } = await loadFixture(deployCarOwnershipFixture);
 
     //build a car
     await carOwnershipContract.buildCar("EW722YG");
